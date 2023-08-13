2b5779a26af723b661eaeb8d4564990ee11ebd51
sanyam <sanyamnarang40@gmail.com>
2023-08-13 13:02:43 +0530

    basics updated

--- 1 - Dart Basics/OOPS/oops.dart
+++ 1 - Dart Basics/OOPS/oops.dart
@@ -1,12 +1,17 @@
 void main() {
   // creating the class object
-  var a = new Student();
-  Student();
+  //var a = new Student();
+  //Student();
 }
 
 class Student {
   String? name;
+  int? rollno;
+
+  Student(String name, int rollno) {
+    this.name = name;
+    this.rollno = rollno;
+  }
 
   // default constructor
-  Student();
 }
--- 1 - Dart Basics/basics-1.dart
+++ 1 - Dart Basics/basics-1.dart
@@ -10,9 +10,8 @@ void main() {
   print(no);
 
   // static declarations
-  int a = 9;
+  int a = 9; // inline declaration
   print('value of a= $a');
-
   int b = 10;
   print('A+B = ${b + a}');
 
@@ -23,4 +22,13 @@ void main() {
   print(name.runtimeType);
   name = false; // Boolean type
   print(name.runtimeType);
+
+  // data types
+  String aa = "abc"; // String type
+  int u = 9; // Integer type
+  num bb = 10; // num can store a number and double
+  BigInt c = BigInt.parse('67777777777'); // Big number
+  bool d = true; // boolean type
+  double e = 7.8; // float type
+  print("$aa + $bb + $c + $d + $e + $u");
 }
