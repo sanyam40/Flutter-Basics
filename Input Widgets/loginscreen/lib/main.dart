16e3ba121c93fdf14234921054428aece48704bd
sanyam <sanyamnarang40@gmail.com>
2024-05-05 14:16:30 +0530

    login screen done

--- Input Widgets/loginscreen/lib/main.dart
+++ Input Widgets/loginscreen/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,33 +14,110 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My App'),
+          title: const Text('Login Page'),
+          backgroundColor: Colors.blueGrey[400],
         ),
         body: SingleChildScrollView(
           child: Container(
-            padding: EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
-                  'SUSTAINIFY',
-                  style: TextStyle(fontSize: 34),
-                ),
-                SizedBox(
+                const SizedBox(
                   height: 34,
                 ),
                 Image.network(
-                    'https://res.cloudinary.com/dtvaexasg/image/upload/v1709278192/flutter/flutter_wv9x3m.svg'),
-                SizedBox(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn8UpzeUZ8uXraZ3XjvO8pFYghl7OiQSvZauchl-7uDQ&s',
+                  height: 100, // Adjust height as needed
+                ),
+                const SizedBox(
                   height: 30,
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Username:'),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Password:'),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                   obscureText: true,
                 ),
-                ElevatedButton(onPressed: () => (), child: Text('Login'))
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add your login logic here
+                  },
+                  child: const Text('Login'),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      // Add your forgot password logic here
+                    },
+                    child: const Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Add your Facebook login logic here
+                      },
+                      icon: const Icon(Icons.facebook),
+                      label: const Text('Login with Facebook'),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Add your Google login logic here
+                      },
+                      icon: Image.network(
+                        "https://w7.pngwing.com/pngs/557/90/png-transparent-google-logo-g-suite-google-text-logo-business.png",
+                        height: 20, // Adjust height as needed
+                      ),
+                      label: const Text('Login with Google'),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add your registration logic here
+                  },
+                  child: const Text('No Account? Register Now'),
+                ),
               ],
             ),
           ),
